35df4c91668ad20d7ab86ef1c0005502e573743c
Diego Surita <suritadev@gmail.com>
2026-02-21 15:46:14 -0300

    build: add php extensions and an entrypoint script to backend service

---
+++ .docker/backend/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+if [ ! -f /var/www/html/.env ] && [ -f /var/www/html/.env.example ]; then
+  cp /var/www/html/.env.example /var/www/html/.env
+fi
+
+composer install
+
+exec "$@"
